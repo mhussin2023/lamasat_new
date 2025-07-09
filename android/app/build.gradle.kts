30ec7c8206ec7352f9124d7367570d8e6155ee66
Programming <mhussin2008@yahoo.com>
2025-07-09 17:20:04 +0300

    updated release 9 on google play added keys

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -15,7 +15,7 @@ if (keystorePropertiesFile.exists()) {
 
 android {
     namespace = "com.nadanewsoft.lamasat_dorrah"
-    compileSdk = flutter.compileSdkVersion
+    compileSdk = 36
     ndkVersion = "28.0.12433566"
 
     compileOptions {
@@ -33,7 +33,7 @@ android {
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
-        targetSdk = flutter.targetSdkVersion
+        targetSdk = 36
         versionCode = flutter.versionCode
         versionName = flutter.versionName
     }
